a5ee8152eb0b1f16bf2e332e0b2e57614f2d0cb6
splatitude <trianglecove@gmail.com>
2024-10-04 15:13:31 -0500

    Load Balancer 3

--- main.tf
+++ main.tf
@@ -53,11 +53,13 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  http_tcp_listeners = {
+  listeners = {
+    ex-http-https-redirect = {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
+  }
 
   target_groups = {
     ex-instance = {
