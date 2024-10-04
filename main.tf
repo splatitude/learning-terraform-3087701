7beed822ad975acaf5cca30ae1cf7871fd34b457
splatitude <trianglecove@gmail.com>
2024-10-04 15:30:36 -0500

    Load Balancer 4

--- main.tf
+++ main.tf
@@ -34,6 +34,19 @@ module "blog_vpc" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  name    = "blog_new"
+  vpc_id  = module.blog_vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
@@ -74,20 +87,6 @@ module "alb" {
   tags = {
     Environment = "dev"
   }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
-  name    = "blog_new"
-  vpc_id  = module.blog_vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
 }
 
 resource "aws_security_group"  "blog" {
